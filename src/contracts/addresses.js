6212882a9ec6080b6fdc884d7e61214432a3e296
GWturbina <shulgagrigory65@gmail.com>
2026-02-18 11:13:23 +0200

    Update addresses.js

--- src/contracts/addresses.js
+++ src/contracts/addresses.js
@@ -8,19 +8,19 @@ const ADDRESSES = {
   USDT: '0x9e5AAC1Ba1a2e6aEd6b32689DFcF62A509Ca96f3', // opBNB Mainnet USDT
 
   // ═══ NSS Core (13 контрактов) ═══
-  CGTToken:          '0x28cC38ebe329D3618f8aeD85a238339128Bd4649',
+  CGTToken:          '0x56F5c29096BB7859e52086118aed31Fe08Fd6659',
   NSTToken:          '0xA1A7BAb7930dBaE59d73918f1fc6c8B9aFD43992',
-  NSSPlatform:       '0x60B5bc57212C531F8b55248cafA8234Ada569A4a',
-  RealEstateMatrix:  '0xdd4AcC46dEb5F0b1f208d5EebC6FCD36cCBCeFAa', // v2.4 + i18n
-  MatrixPaymentsV2:  '0xB488f30BF3e619b98DED8764351209a5c510EEA8',
-  SwapHelper:        '0x9C7d2FF4CBD45c782EF6e310ed41AFB1A2cf6402',
+  NSSPlatform:       '0xFb1ddFa8A7EAB0081EAe24ec3d24B0ED4Dd84f2B',
+  RealEstateMatrix:  '0xCA10A6971C9Dcbda4e8AdFC6328977261B8B82Fe', // v2.4 + i18n
+  MatrixPaymentsV2:  '0x959217Aab60f01cc582373E1a2bc36e7a076bc39',
+  SwapHelper:        '0xFF0e9BFFf1cc5A6B65f689bF2442056627686Bf5',
   CharityFund:       '0x090EEc8b07805310b06FD3d9198c8947E7067A59',
-  HousingFund:       '0x04CB8306887Bd758D21Dcb6abaBd205edA8eD333',
-  CardGiftMarketing: '0x6354c3f198d89C43EfeAA5A5f3a0a6be8cd688fC',
-  GemVault:          '0x5214CCeE57FB8D6BFfA8622B1eeB64c1fdD8d774',
-  SafeVault:         '0x768A3232d511Dc7EED2f59332a0131C3821cf630',
-  AICredits:         '0x171673F09db0375345d6Fa6fE23a5A99c1Fc5668',
-  P2PEscrow:         '0xB99bD23299a4AaeaAB85A96e67DB0c9b5E4ed6df',
+  HousingFund:       '0x1d2aF2A868fCC547c3F9339AeC3e2DfaA533bE92',
+  CardGiftMarketing: '0x67dD9ed3E63bA44047A70DA70AeC508101F048b7',
+  GemVault:          '0x9b0fA988b0fb5Fe79332f66aB1F7a86584a1fd47',
+  SafeVault:         '0x3263652D9D1f47F3bC211A7ecD1f5DEeE632Ce3f',
+  AICredits:         '0xE208dc14A5033Bf30047edA0b7b54Cb87836318D',
+  P2PEscrow:         '0xC0DA8bFEA02B034260939ccF5fe8cce55486e62c',
 
   // ═══ GlobalWay ═══
   GlobalWay:         '0xe8e2af46AEEec1B51B335f10C5912620B1a2707F',
